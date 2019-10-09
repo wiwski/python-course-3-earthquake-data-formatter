6bbeaa0fdeb7729cf103707b22f9bf94e17b2200
Witold Wroblewski <witoldwrob@gmail.com>
2019-10-09 17:01:34 +0200

    Add oop feature

---
+++ sism/models.py
@@ -0,0 +1,10 @@
+""" TODO: Implémentez deux classes Earthquake et Catalog
+
+Earthquake comporte les attributs time, latitude, longitude, depth, mag
+que l'on peut passé comme arguments lorsqu'on instancie la classe.
+
+Catalog est une classe représentant un 
+"""
+
+class Catalog(list):
+    pass
